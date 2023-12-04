78c53c6b9442c0b550b025f6a30a192fe006d6ca
pangil5634 <oksk6681@naver.com>
2023-12-05 04:10:11 +0900

    231205 광일 : Div.js / Long.js 파일 추가

---
+++ bingo/src/Components/Div.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Div = styled.div `
+    display: ${props => props.display || "flex"};
+    justify-content: ${props => props.justifyContent || "center"};
+    flex-direction: ${props => props.flexDirection || "row"};
+    align-items: ${props => props.alignItems || "center"};
+    width : ${props => props.width || "100%"};
+    height : ${props => props.height || "200px"};
+    border : ${props => props.border || "1px solid black"};
+    box-sizing : ${props => props.boxSizing || "border-box"};
+    background-color: ${props => props.backgroundColor || " black"};
+    color: ${props => props.backgroundColor || " white"};
+    padding : ${props => props.padding || "10px"};
+    margin : ${props => props.margin || "0px"};
+    position: ${props => props.position || ''};
+    background-image: ${props => props.backgroundImage || ''};
+
+`;
---
+++ bingo/src/Components/Long.js
@@ -0,0 +1,8 @@
+
+function Long() {
+  return (
+      <h3> 속보 : BINGO 롱커톤 1위 수상</h3>
+  );
+}
+
+export default Long;
