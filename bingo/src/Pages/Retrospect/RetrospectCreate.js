7b900f505b9f131ac25c06ff3ebff07088ed18ec
pangil5634 <oksk6681@naver.com>
2023-12-21 22:42:21 +0900

    231221 광일 : 수정 중 PR

--- bingo/src/Pages/Retrospect/RetrospectCreate.js
+++ bingo/src/Pages/Retrospect/RetrospectCreate.js
@@ -1,28 +1,146 @@
 import {useState} from "react";
-import {Button, Input} from "../../Components/NormalComponents/Form";
+import {Button, Fieldset, Input, Legend} from "../../Components/NormalComponents/Form";
 import {Div} from "../../Components/NormalComponents/Section";
 import {Label} from "../../Components/NormalComponents/Text";
 import {KPT, FFF} from '../../Preset/RetrospectWaysPreset';
+
+function RadioCard({value, label, selectedValue, onChange}) {
+    return (
+        <label>
+            <input
+                type='radio'
+                name='fruits'
+                value={value}
+                checked={value === selectedValue}
+                onChange={onChange}
+                style={{
+                    display: 'none'
+                }}/>
+            <Div
+                width="150px"
+                height="100px"
+                border="1px solid black"
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+                cursor="pointer"
+                borderRadius="15px"
+                margin="5px"
+                backgroundColor={value === selectedValue
+                    ? '#e0e0e0'
+                    : 'white'}
+                onClick={() => onChange(value)}>
+                {label}
+            </Div>
+        </label>
+    );
+}
+
+function KPTQuestion() {
+    return (
+        <Div flexDirection="column">
+            <Fieldset height="auto">
+                <Legend>Keep</Legend>
+                <Button width="100px" height="40px" borderRadius="15px" fontSize="15px">질문 추가하기</Button>
+            </Fieldset>
+            <Fieldset height="auto">
+                <Legend>Problem</Legend>
+                <Button width="100px" height="40px" borderRadius="15px" fontSize="15px">질문 추가하기</Button>
+            </Fieldset>
+            <Fieldset height="auto">
+                <Legend>Try</Legend>
+                <Button width="100px" height="40px" borderRadius="15px" fontSize="15px">질문 추가하기</Button>
+            </Fieldset>
+        </Div>
+    );
+}
+
+function FFFQuestion() {
+    return (
+        <Div flexDirection="column">
+            <Fieldset height="auto">
+                <Legend>Fact</Legend>
+                <Button width="100px" height="40px" borderRadius="15px" fontSize="15px">질문 추가하기</Button>
+            </Fieldset>
+            <Fieldset height="auto">
+                <Legend>Feeling</Legend>
+                <Button width="100px" height="40px" borderRadius="15px" fontSize="15px">질문 추가하기</Button>
+            </Fieldset>
+            <Fieldset height="auto">
+                <Legend>Future Action</Legend>
+                <Button width="100px" height="40px" borderRadius="15px" fontSize="15px">질문 추가하기</Button>
+            </Fieldset>
+        </Div>
+    );
+}
 function RetrospectCreate() {
 
     // 질문을 모아놓는 변수
-    const [questions, setQuestions] = useState([]);
+    const [questions1, setQuestions1] = useState([]);
+    const [questions2, setQuestions2] = useState([]);
+    const [questions3, setQuestions3] = useState([]);
 
     // 질문을 추가하는 변수
-    const addQuestion = (type) => {
+    const addQuestion1 = (type) => {
         // Adding a new question to the state with a specified type
-        setQuestions([
-            ...questions, {
-                id: questions.length + 1,
+        setQuestions1([
+            ...questions1, {
+                id: questions1.length + 1,
+                type
+            }
+        ]);
+    };
+    const addQuestion2 = (type) => {
+        // Adding a new question to the state with a specified type
+        setQuestions2([
+            ...questions2, {
+                id: questions2.length + 1,
+                type
+            }
+        ]);
+    };
+    const addQuestion3 = (type) => {
+        // Adding a new question to the state with a specified type
+        setQuestions1([
+            ...questions3, {
+                id: questions3.length + 1,
                 type
             }
         ]);
     };
 
+    const [selectedFruit, setSelectedFruit] = useState('Custom');
+    const handleRadioChange = (value) => {
+        // Handle the radio change logic here
+        setSelectedFruit(value);
+    };
+
     return (
         <Div flexDirection="column" padding="10px">
             <h2>회고 생성 페이지입니다</h2>
-            <Div margin="0px 0px 10px 0px">
+            <Div>
+                <RadioCard
+                    value='KPT'
+                    label='KPT'
+                    selectedValue={selectedFruit}
+                    onChange={handleRadioChange}/>
+                <RadioCard
+                    value='FFF'
+                    label='FFF'
+                    selectedValue={selectedFruit}
+                    onChange={handleRadioChange}/>
+                <RadioCard
+                    value='Custom'
+                    label='Custom'
+                    selectedValue={selectedFruit}
+                    onChange={handleRadioChange}/>
+            </Div>
+
+            {/* 선택된 과일 출력 */}
+            <Div marginTop="10px">
+                선택된 과일: {selectedFruit}
+            </Div>
+            <KPTQuestion/> {/* <Div margin="0px 0px 10px 0px">
                 <Button
                     width="100px"
                     height="40px"
@@ -67,9 +185,9 @@ function RetrospectCreate() {
                         }
                     </div>
                 ))
+            } */
             }
-
-            <Div>
+            < Div >
                 <Button width="100px" height="40px" borderRadius="15px" fontSize="15px">회고 생성하기</Button>
             </Div>
 
@@ -79,9 +197,3 @@ function RetrospectCreate() {
 }
 
 export default RetrospectCreate;
-
-// <Div>                                     <Label margin="0px 10px 0px 0px"
-// width="20%">질문 제목</Label>                                     <Input
-// type="text"                                         placeholder="회고 제목을
-// 입력해주세요."                                         border="2px solid blue"
-// width="80%">                                     </Input> <Div />
