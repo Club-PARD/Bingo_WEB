df740340eb58baa69791cc313a3893323fe09168
Cocomong98 <yonghjjm0618@gmail.com>
2023-12-24 05:28:35 +0900

    231224 용현 : 바뀐 구조에 따른 Readme 변경

--- bingo/src/Pages/Retrospect/RetrospectWrite.js
+++ bingo/src/Pages/Retrospect/RetrospectWrite.js
@@ -5,7 +5,7 @@ import Editor from "./Editor";
 function RetrospectWrite() {
     return (
         // 전체 총괄 Div
-        <Div alignItems="center" justifyContent="center" flexDirection="column">
+        <Div alignItems="center" justifyContent="center" flexDirection="column" backgroundColor="whitesmoke" height="100vh">
             <P fontSize="30px" fontWeight="bolder">회고 작성하기</P>
             <Editor />
         </Div>
