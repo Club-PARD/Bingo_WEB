a75260ce89e1a5e59961340f3154813877ca07ab
Cocomong98 <yonghjjm0618@gmail.com>
2023-12-24 02:09:26 +0900

    231223 용현 : 회고 작성 페이지 틀 구성

--- bingo/src/Pages/Retrospect/RetrospectWrite.js
+++ bingo/src/Pages/Retrospect/RetrospectWrite.js
@@ -1,7 +1,11 @@
+import { Div } from "../../Components/NormalComponents/Section";
+import { P } from "../../Components/NormalComponents/Text";
+
 function RetrospectWrite() {
     return (
-        <Div>
-            <P>님의 회고입니다.</P>
+        // 전체 총괄 Div
+        <Div alignItems="center" justifyContent="center">
+            <P fontSize="30px" fontWeight="bolder">회고 작성하기</P>
         </Div>
     );
 }
