943a3c863b21af64fc0ba687b9c1b738d5c60beb
pangil5634 <oksk6681@naver.com>
2023-12-24 22:47:21 +0900

    231224 광일 : 충돌 수정

--- bingo/src/Pages/Retrospect/RetrospectWrite.js
+++ bingo/src/Pages/Retrospect/RetrospectWrite.js
@@ -1,13 +1,18 @@
-import { Div } from "../../Components/NormalComponents/Section";
-import { P } from "../../Components/NormalComponents/Text";
+import {Div} from "../../Components/NormalComponents/Section";
+import {P} from "../../Components/NormalComponents/Text";
 import Editor from "./Editor";
 
 function RetrospectWrite() {
     return (
         // 전체 총괄 Div
-        <Div alignItems="center" justifyContent="center" flexDirection="column" backgroundColor="whitesmoke" height="100vh">
+        <Div
+            alignItems="center"
+            justifyContent="center"
+            flexDirection="column"
+            backgroundColor="whitesmoke"
+            height="100vh">
             <P fontSize="30px" fontWeight="bolder">회고 작성하기</P>
-            <Editor />
+            <Editor/>
         </Div>
     );
 }
