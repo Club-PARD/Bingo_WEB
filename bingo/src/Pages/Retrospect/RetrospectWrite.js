d571d743dad2e89c7031dddc4e96c073678b156d
pangil5634 <oksk6681@naver.com>
2023-12-25 15:37:33 +0900

    231225 광일 : 임시 커밋

--- bingo/src/Pages/Retrospect/RetrospectWrite.js
+++ bingo/src/Pages/Retrospect/RetrospectWrite.js
@@ -1,13 +1,21 @@
 import { Div } from "../../Components/NormalComponents/Section";
 import { P } from "../../Components/NormalComponents/Text";
+<<<<<<< HEAD
 import Editor from "./Editor";
+=======
+>>>>>>> a75260c (231223 용현 : 회고 작성 페이지 틀 구성)
 
 function RetrospectWrite() {
     return (
         // 전체 총괄 Div
+<<<<<<< HEAD
         <Div alignItems="center" justifyContent="center" flexDirection="column" backgroundColor="whitesmoke" height="100vh">
             <P fontSize="30px" fontWeight="bolder">회고 작성하기</P>
             <Editor />
+=======
+        <Div alignItems="center" justifyContent="center">
+            <P fontSize="30px" fontWeight="bolder">회고 작성하기</P>
+>>>>>>> a75260c (231223 용현 : 회고 작성 페이지 틀 구성)
         </Div>
     );
 }
