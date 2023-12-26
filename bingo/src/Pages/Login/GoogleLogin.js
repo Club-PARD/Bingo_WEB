a0cf497fd25f2afb8762a5642030e2767c9c4e11
Cocomong98 <yonghjjm0618@gmail.com>
2023-12-26 15:05:16 +0900

    231226 용현 : Login 오류파일 삭제

--- bingo/src/Pages/Login/GoogleLogin.js
+++ bingo/src/Pages/Login/GoogleLogin.js
@@ -4,9 +4,15 @@ import { useNavigate } from "react-router-dom";
 import { login } from "../../Api/AuthApi";
 import { jwtDecode } from 'jwt-decode';
 
-const LoginDummy = {
-    
-}
+const LoginDummy = [
+    {
+        email: "",
+        email_verified: "",
+        name: "",
+        jti: "",
+        picture: "",
+    },
+];
 
 const GoogleLoginButton = () => {
     const navigate = useNavigate();
@@ -18,7 +24,18 @@ const GoogleLoginButton = () => {
                     onSuccess={(credentialResponse) => {
                         const decodedToken = jwtDecode(credentialResponse.credential);
                         console.log(decodedToken);
+                        
+                        // 받아온 값을 기반으로 데이터 갱신하기
+                        const updatedData = LoginDummy.map(dummy => {
+                            return Object.keys(dummy).reduce((acc, key) => {
+                                acc[key] = decodedToken[key] || dummy[key];
+                                return acc;
+                            }, {});
+                        });
+                        // 성공이라고 콘솔에 남기기
                         console.log("로그인 성공!");
+                        // api로 정보 넘겨주기
+                        // 페이지 이동
                         navigate("/Home");
                     }}
                     onError={() => {
