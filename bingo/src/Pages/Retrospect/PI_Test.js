bf2cae3c3ce505dbac0cfe229aaa4553789b3b95
pangil5634 <oksk6681@naver.com>
2023-12-26 15:33:16 +0900

    231226 광일 : Recoil get/patch 구현

--- bingo/src/Pages/Retrospect/PI_Test.js
+++ bingo/src/Pages/Retrospect/PI_Test.js
@@ -1,52 +1,98 @@
 import {Div} from "../../Components/NormalComponents/Section";
-import React from 'react';
-import {countState, titleState} from '../../Contexts/Atom';
-import { useRecoilState, useRecoilValue, useSetRecoilState, useResetRecoilState } from 'recoil';
+import React, {useEffect} from 'react';
+import {countState, titleState, contentState} from '../../Contexts/Atom';
+import {useRecoilState, useSetRecoilState, useResetRecoilState} from 'recoil';
 import {Link} from "react-router-dom";
+import {Input, Textarea} from '../../Components/NormalComponents/Form';
+import axios from "axios";
 
 function PI_Test() {
-    // useState와 같지만, useRecoilState을 사용하여 다른 파일에 있는 아톰을 읽을 수 있다.
-    const [counter, setCounter] = useRecoilState(countState);
+    // Recoil state를 통해 상태를 관리
+    const [count, setCount] = useRecoilState(countState);
     const [title, setTitle] = useRecoilState(titleState);
+    const [content, setContent] = useRecoilState(contentState);
+    const counterHandler = useSetRecoilState(countState);
+    const resetCounter = useResetRecoilState(countState);
 
-    // const currentCount = useRecoilValue(countState);  읽기 전용!
-
-    const counterHandler = useSetRecoilState(countState); // 값만 변경 시키기
-
-    const resetCounter = useResetRecoilState(countState); // 디폴트값으로 값 변경
-
+    // 숫자를 증가시키는 함수
     const plusCount = () => {
         counterHandler((pre) => pre + 1);
     };
+
+    // 숫자를 감소시키는 함수
     const minusCount = () => {
         counterHandler((pre) => pre - 1);
     };
+
+    // 입력 필드의 변경에 따라 타이틀을 업데이트하는 함수
     const handleInputChange = (event) => {
         setTitle(event.target.value);
     };
-    return (
-        <Div flexDirection = "column">
-            <div>
-                {/* <div>{counter}</div> */}
-                {/* counter 또는 currentCount 둘 중 하나를 사용해도 상관없는거 같다. */}
-                <div>{counter}</div>
-                {/* 그러나 읽기만 하려고 currentCount를 사용했다. */}
-                {/* <button onClick={() => setCounter((num) => num + 1)}>+</button>
-      <button onClick={() => setCounter((num) => num - 1)}>-</button> */
-                }
-                {/* 위의 코드도 작동한다. */}
 
+    // 내용 입력 필드의 변경에 따라 컨텐츠를 업데이트하는 함수
+    const handleContentChange = (e) => {
+        setContent(e.target.value);
+    }
+
+    const getData = async () => {
+        try {
+            const data = await axios.get("http://172.18.157.205:8080/api/v1/auth/test");
+            setContent(data.data.content);
+            setTitle(data.data.title);
+            setCount(data.data.test);
+            console.log("성공입니다.");
+        } catch (error) {
+            console
+                .log
+                .apply(error);
+        }
+    }
+
+    // 서버로 PATCH 요청을 보내어 데이터를 업데이트하는 함수
+    const updateData = async () => {
+        try {
+            const dataSet = {
+                title: title,
+                content: content,
+                test: count
+            }
+            // 서버의 엔드포인트와 PATCH 메서드 사용
+            const data = await axios.patch(
+                "http://172.18.157.205:8080/api/v1/auth/test",
+                dataSet
+            );
+            console.log(data);
+            console.log("성공입니다.");
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    // 컴포넌트가 처음 마운트될 때 한 번만 실행
+    useEffect(() => {
+        getData();
+        updateData();
+    }, []);
+
+    return (
+        <Div flexDirection="column">
+            <div>
+                {/* 현재 숫자 표시 */}
+                <div>{count}</div>
+                {/* 숫자 증가, 감소, 리셋 버튼 */}
                 <button onClick={plusCount}>+</button>
                 <button onClick={minusCount}>-</button>
                 <button onClick={resetCounter}>reset</button>
                 <br/>
-                <br/>
-                <label>텍스트 :
-                </label>
+                <br/> {/* 타이틀 입력 필드 */}
+                <label>타이틀 :</label>
                 <input type="text" value={title} onChange={handleInputChange}></input>
+                <br/>
+                <label>컨텐츠 :</label>
+                <input type="text" value={content} onChange={handleContentChange}></input>
             </div>
-            <br/>
-            <Link to = "/PI2">변경되었는지 보러 가기</Link>
+            <br/> {/* 다른 페이지로 이동하는 링크 */}
+            <Link to="/PI2">변경되었는지 보러 가기</Link>
         </Div>
     );
 }
