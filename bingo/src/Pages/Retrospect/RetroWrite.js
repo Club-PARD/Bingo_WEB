aa21ff8c0fd1cec0a9facc8f1d31c34a486eb259
Cocomong98 <yonghjjm0618@gmail.com>
2023-12-28 06:56:11 +0900

    231228 용현 : 더미데이터 사용 회고 작성 페이지

---
+++ bingo/src/Pages/Retrospect/RetroWrite.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const RetroWrite = ({ name, desc }) => (
+  <div>
+    <h2>{name}</h2>
+    <p>{desc}</p>
+  </div>
+);
+
+export default RetroWrite;
--- bingo/src/Pages/Retrospect/RetrospectWriteText.js
+++ bingo/src/Pages/Retrospect/RetrospectWriteText.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 import styled from "styled-components";
 import Breadcrumb from "../../Breadcrumb";
+// import RetroWrite from "./RetroWrite";
 
 // 전체를 감싸는 div, 이 아래에 Header / Body / Footer로 나뉘어 있음
 const Whole = styled.div`
@@ -29,17 +30,47 @@ const Footer = styled.div`
 `
 
 // Body 안에 들어가는 회고 작성칸을 감싸는 테두리
-const DottedBorder = styled.div`
+const Border = styled.div`
     border : 5px solid gray;
     border-radius : 25px;
-    margin : 0.5%;
+    margin : 1%;
     height : 100%;
     background-color : white;
 `
+const BorderInside = styled.div`
+    border : 2px solid red;
+    padding : 1%;
+`
+const RetroType = styled.div`
+    display : flex;
+    flex-direction : row;
+    align-items : end;
+`
+const RetroABC = styled.div`
+    /* font-weight : bold; */
+    font-size : 100px;
+    padding-right : 1%;
+`
+
 // Footer 안에 들어갈 버튼들의 Preset
 const Btn = styled.button`
     height : 100%;
 `
+
+const RetrospectData = [
+    {
+        name : "아이디에이션 할 때 유지할만한 점은 없었나요?",
+        desc : "",
+    },
+    {
+        name : "프로젝트를 하면서 유지할만한 점은 없었나요?",
+        desc : "",
+    },
+    {
+        name : "개발 과정에서 유지할만한 점은 없었나요?",
+        desc : "",
+    },
+];
 
 function RetrospectWriteText() {
     const handleNextClick = () => {
@@ -53,11 +84,26 @@ function RetrospectWriteText() {
                 {/* Breadcrumb은 현재 위치에 따라 달라진다 / 현위치 : 1 (회고 작성하기) */}
                 <Breadcrumb activeKey={1} />
             </Header>
+            {/* 회고 작성 창 */}
             <Body>
-                <DottedBorder>
-                    <p>회고 작성하기</p>
-                </DottedBorder>
+                <Border>
+                    <BorderInside>
+                        {/* 회고 종류와 풀네임 */}
+                        <RetroType>
+                            <RetroABC>K</RetroABC>
+                            <h1>Keep</h1>
+                        </RetroType>
+                    </BorderInside>
+                    {/* RetrospectData의 각 항목에 대해 RetroWrite 컴포넌트를 렌더링 */}
+                    {RetrospectData.map((retro, index) => (
+                        <div>
+                            <h2>{retro.name}</h2>
+                            <p>{retro.desc}</p>
+                        </div>
+                    ))}
+                </Border>
             </Body>
+            {/* 취소 다음 버튼 */}
             <Footer>
                 <Btn onClick={handleNextClick}>
                     다음
