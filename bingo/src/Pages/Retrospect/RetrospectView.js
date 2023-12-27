0b874e273e2986e11400d73c7e9301e3280753dc
이지윤 <12jiyun@gmail.com>
2023-12-28 07:12:07 +0900

    restropectView

--- bingo/src/Pages/Retrospect/RetrospectView.js
+++ bingo/src/Pages/Retrospect/RetrospectView.js
@@ -10,7 +10,36 @@ function RetrospectView() {
                 <Breadcrumb activeKey={3} />
             </Header>
             <Body>
-                
+                {/*
+                구조- body는 overflow : auto;이다
+                1. 큰 div n개 -> 회고의 step을 표현해줄 so flex-direction : row
+                    1-1 div2개 -> 1개는 initial, 1개는 full word
+                2. 큰 div내부에 작은 div n+1개 (n==생성자가 생성한 질문 개수)
+                    2-1 첫번째 div는 생성자가 작성한 질문을 bold처리해서 출력
+                    2-2 이후의 n개의 div는 또 쪼개짐 2개
+                        2-2-1  
+                */}
+                <StepDiv>
+                    <StepInitial>K</StepInitial>
+                    <StepFullWord>Keep</StepFullWord>
+                </StepDiv>
+                <InnerDiv>
+                    <QuestionDiv>아이디에이션</QuestionDiv>
+                    <OuterAnswer>
+                        <AnswerDiv>
+                            <AnswerText>아이디에이션</AnswerText>
+                            <AnswerDate>23.12.28</AnswerDate>
+                        </AnswerDiv>
+                        <Eclipse />
+                    </OuterAnswer>
+                    <OuterAnswer>
+                        <AnswerDiv>
+                            <AnswerText>아이디에이션아이디에이션아이디에이션아이디에이션아이디에이션아이디에이션아이디에이션아이디에이션아이디에이션아이디에이션아이디에이션</AnswerText>
+                            <AnswerDate>23.12.28</AnswerDate>
+                        </AnswerDiv>
+                        <Eclipse />
+                    </OuterAnswer>
+                </InnerDiv>
             </Body>
         </Whole>
     );
@@ -36,7 +65,7 @@ const Body = styled.div`
     flex설정은 어떻게 되었는가?
     */
     height : 96.5%;
-    width: 100%;
+    width: 92%;
 
     /* background-color : whitesmoke; */
 
@@ -49,8 +78,84 @@ const Body = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: space-evenly;
+    justify-content: top;
     overflow: auto;
-    background-color: aquamarine;
+    margin: 0 4%;
 `
-
+//회고내부의 단계(ex.K,P,T)를 나타내는 div
+const StepDiv = styled.div`
+    height: 18%;
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+`
+const StepInitial = styled.div`
+    height: 100%;
+    width: 5%;
+    font-size: 147px;
+    display: flex;
+    justify-content: center;
+    margin-top: 2%;
+`
+const StepFullWord = styled.div`
+    height: 100%;
+    width: 30%;
+    font-size: 147px;
+    display: flex;
+    justify-content: left;
+    align-items: end;
+    margin: 0.2%;
+    color: #737373;
+    font-size: 52px;
+`
+//질문과 답변을 감싸주는 div
+const InnerDiv = styled.div`
+    width: 100%;
+    height: auto;
+    margin-top: 4%;
+    display: flex;
+    flex-direction: column;
+`
+const QuestionDiv = styled.div`
+    width: 100%;
+    height: 130%;
+    align-items: center;
+    font-size: 36px;
+    font-weight: 700;
+`
+const OuterAnswer = styled.div`
+    width: 100%;
+    height: 130%;
+    display: flex;
+    flex-direction: row;
+    align-items: top;
+    margin-top: 1%;
+`
+const AnswerDiv = styled.div`
+    width: 86%;
+    height: auto;
+    background-color: #EAEAEA;
+    display: flex;
+    flex-direction: row;
+`
+const AnswerText = styled.div`
+    width: 65%;
+    height: auto;
+    font-size: 36px;
+    display: flex;
+    align-items: center;
+`
+const AnswerDate = styled.div`
+    width: 34%;
+    height: auto;
+    font-size: 25px;
+    color: #C9C9C9;
+    margin-left: 1%;
+`
+const Eclipse = styled.div`
+    width: 5%;
+    height: 66px;
+    background-color: #EAEAEA;
+    border-radius: 50%;
+    margin-left: 2%;
+`
