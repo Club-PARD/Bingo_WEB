ead7338a8b16388ac93b213f5182a42b8f1b601c
pangil5634 <oksk6681@naver.com>
2023-12-28 01:02:36 +0900

    231228 광일 : WorkspaceView에 남아 있던 Modal 관련 내용 삭제

--- bingo/src/Pages/Workspace/WorkspaceView.js
+++ bingo/src/Pages/Workspace/WorkspaceView.js
@@ -1,28 +1,13 @@
 import {Div} from "../../Components/NormalComponents/Section";
-import {Img} from "../../Components/NormalComponents/Etc";
-import Modal from "react-modal";
-import {useState, React} from "react";
-import {Button} from "../../Components/NormalComponents/Form";
-import {CopyToClipboard} from 'react-copy-to-clipboard';
-import {Link, useLocation} from "react-router-dom";
+import {React} from "react";
 import RetrospectInWorkspace from "./RetrospectInWorkspace";
 import BingoBoard from "../../Preset/WorkspacePreset/BingoBoard";
 import styled from "styled-components";
 import {P} from "../../Components/NormalComponents/Text";
 
-
 // workspace에 들어오면 보이는 화면 아직 와이어 프레임 안나와서 정확한건 미정 빙고페이지로 이동 가능 회고생성페이지로 이동 가능
 // RetrospectInWorkspace component출력 회고결과 출력(이것도 디자인이 완성되고 백엔드가 연결되어야 가능하다)
 function WorkspaceView() {
-    const [modalIsOpen1, setModalIsOpen1] = useState(false);
-    const [value, setValue] = useState("12345678");
-    const openModal1 = () => {
-        setModalIsOpen1(true);
-    };
-
-    const closeModal = () => {
-        setModalIsOpen1(false);
-    };
     return (
         <Div width="100%" height="100%">
 
@@ -55,75 +40,6 @@ function WorkspaceView() {
                 </Section_Retrospect>
 
             </Div>
-
-            {/*
-            Modal size: width:584px, height:372px
-            Modal show invite code to user, user can copy the code to click "복사 " button*/
-            }
-
-            <Modal
-                isOpen={modalIsOpen1}
-                onRequestClose={closeModal}
-                style={{
-                    overlay: {
-                        backgroundColor: "rgba(0, 0, 0,0.5)"
-                    },
-                    content: {
-                        borderRadius: "12px",
-                        padding: 0,
-                        color: "black",
-                        background: `#D9D9D9`,
-                        backgroundSize: "cover",
-                        // backgroundRepeat: "no-repeat",
-                        margin: "0",
-                        width: "35%",
-                        height: "39%",
-                        display: "flex",
-                        border: "none",
-                        //alignItems: "center",
-                        overflowY: "hidden",
-
-                        display: "flex",
-                        flexDirection: "column",
-                        overflowY: "auto",
-
-                        //position: 'absolute',  absolute positioning
-
-                        transform: "translate(87%, 60%)", // center the modal
-                        // 모달 내용이 부모 요소의 높이를 초과하면 자동으로 스크롤 바를 생성하도록 설정합니다. "overflowY: 'auto'"가 그 역할을
-                        // 담당합니다. 또한, 모달의 높이(height)를 조정하여 모달의 내용이 충분하지 않을 경우 모달 자체의 높이를 줄일 수 있습니다.
-                    }
-                }}>
-                {/* 이미지 추가 modal's width=1179, height=616 */
-                }
-                <Div
-                    width="100%"
-                    height="10%"
-                    alignItems="center"
-                    flexDirection="row-reverse"
-                    padding="2% 2% 0 0">
-                    <Img
-                        src="/img/Home/close.png"
-                        alt="Close"
-                        onClick={closeModal}
-                        width="4%"
-                        height="90%"/>
-                </Div>
-                <Div alignItems="center" width="100%" height="70%" flexDirection="column">
-                    <Div fontSize="20px" fontWeight="400" margin="0 0 12% 0">팀원 초대하기 코드</Div>
-                    <Div fontSize="75px">{value}</Div>
-                </Div>
-                <CopyToClipboard text={value} onCopy={() => alert("클립보드에 복사되었습니다.")}>
-                    <Button
-                        width="99px"
-                        height="30px"
-                        style={{
-                            marginLeft: "40%"
-                        }}
-                        backgroundColor="#959595"
-                        borderRadius="9px">복사</Button>
-                </CopyToClipboard>
-            </Modal>
         </Div>
     );
 }
