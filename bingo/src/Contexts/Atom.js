fd86c30524e9ea926fcac5880df4a01acca6a8d8
이지윤 <12jiyun@gmail.com>
2023-12-29 03:10:12 +0900

    231229 지윤 : 빙고 팀evaluation recoil처리

--- bingo/src/Contexts/Atom.js
+++ bingo/src/Contexts/Atom.js
@@ -10,3 +10,27 @@ export const contentState = atom({ key: "content", default: '' });
 export const ProjectTitleState = atom({ key: "projectTitle", default: ''});
 export const ProjectDescState = atom({ key: "projectDesc", default: ''});
 export const ProjectSelectedFileState = atom({ key: "projectSelectedFile", default: ''});
+
+//team evaluation in BingoBoard
+export const ChipData = atom({
+  key: 'chipData',
+  default: [
+    { key: 0, label: '존중하는', flag: false },
+    { key: 1, label: '열정적인', flag: false },
+    { key: 2, label: '도전적인', flag: false },
+    { key: 3, label: '의사소통이 잘 되는', flag: false },
+    { key: 4, label: '성실한', flag: false },
+    { key: 5, label: '인내심있는', flag: false },
+    { key: 6, label: '화목한', flag: false },
+    { key: 7, label: '분위기 좋은', flag: false },
+    { key: 8, label: '칭찬하는', flag: false },
+  ],
+});
+export const UserList = atom({
+  key: 'userList',
+  default: [
+    { name: '유저1', value: 0, chipData: ChipData },
+    { name: '유저2', value: 1, chipData: ChipData },
+    { name: '유저3', value: 3, chipData: ChipData },
+  ],
+});
--- bingo/src/Pages/Retrospect/Components/Chips.js
+++ bingo/src/Pages/Retrospect/Components/Chips.js
@@ -2,6 +2,8 @@ import * as React from 'react';
 import { styled } from '@mui/material/styles';
 import Chip from '@mui/material/Chip';
 import Paper from '@mui/material/Paper';
+import { ChipData, UserList } from '../../../Contexts/Atom';
+import { useRecoilState } from "recoil";
 
 const ListItem = styled('li')(({ theme }) => ({
   margin: theme.spacing(0.5),
@@ -9,17 +11,30 @@ const ListItem = styled('li')(({ theme }) => ({
 
 // 실제 칩에 들어갈 내용
 export default function Chips() {
+  const [chipData, setChipData] = useRecoilState(ChipData);
+  const [usrList, setUserList] = useRecoilState(UserList);
+  /*
   const [chipData, setChipData] = React.useState([
-    { key: 0, label: '존중하는' },
-    { key: 1, label: '열정적인' },
-    { key: 2, label: '도전적인' },
-    { key: 3, label: '의사소통이 잘 되는' },
-    { key: 4, label: '성실한' },
-    { key: 5, label: '인내심있는' },
-    { key: 6, label: '화목한' },
-    { key: 7, label: '분위기 좋은' },
-    { key: 8, label: '칭찬하는' },
+    { key: 0, label: '존중하는', flag: 'false'},
+    { key: 1, label: '열정적인', flag: 'false' },
+    { key: 2, label: '도전적인', flag: 'false' },
+    { key: 3, label: '의사소통이 잘 되는', flag: 'false' },
+    { key: 4, label: '성실한', flag: 'false' },
+    { key: 5, label: '인내심있는', flag: 'false' },
+    { key: 6, label: '화목한', flag: 'false' },
+    { key: 7, label: '분위기 좋은', flag: 'false' },
+    { key: 8, label: '칭찬하는', flag: 'false' },
   ]);
+  */
+  const handleClick = (index) => {
+    setChipData(chipData.map((chip, i) => 
+      i === index ? { ...chip, flag: chip.flag === 'true' ? 'false' : 'true' } : chip
+    ));
+  };  
+  //flag가 잘 바뀌는지 확인
+  React.useEffect(() => {
+    console.log(chipData[1].flag);
+  }, [chipData[1].flag]);
 
   const handleDelete = (chipToDelete) => () => {
     setChipData((chips) => chips.filter((chip) => chip.key !== chipToDelete.key));
@@ -38,13 +53,16 @@ export default function Chips() {
       }}
       component="ul"
     >
-      {chipData.map((data) => {
+      {chipData.map((data, index) => {
+        const chipBackgroundColor = data.flag === 'true' ? '#E7E7E7' : '#323232';
+        const chipTextColor = data.flag === 'true' ? '#000000' : '#FFFFFF';
+
         return (
           <ListItem key={data.key}>
             <Chip
               label={data.label}
-              color="primary"
-              sx={{ fontSize: '50px', margin: '0.5rem', height: '100%', borderRadius: '30px'}}
+              onClick={() => handleClick(index)}
+              sx={{ fontSize: '50px', margin: '0.5rem', height: '100%', borderRadius: '30px', backgroundColor: chipBackgroundColor, color: chipTextColor}}
             />
           </ListItem>
         );
